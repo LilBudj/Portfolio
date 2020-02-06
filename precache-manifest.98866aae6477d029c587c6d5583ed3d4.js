8d7ac1c15c53cd160879e29f29292fb1d6f1ceb0
LilBudj <kinich2001@gmail.com>
2020-02-06 18:13:38 +0300

    Updates

---
+++ precache-manifest.98866aae6477d029c587c6d5583ed3d4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6e87cf0425b9617f6024e4c329a969",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e507c000a15d383e5233",
+    "url": "/Portfolio/static/css/main.2fb38249.chunk.css"
+  },
+  {
+    "revision": "9912a36e96fc5d5c7555",
+    "url": "/Portfolio/static/js/2.312f1d7c.chunk.js"
+  },
+  {
+    "revision": "e507c000a15d383e5233",
+    "url": "/Portfolio/static/js/main.f41e88d7.chunk.js"
+  },
+  {
+    "revision": "89b1ca4e20787a4cf849",
+    "url": "/Portfolio/static/js/runtime-main.39abcd15.js"
+  },
+  {
+    "revision": "39a7d16b50a89daceec228b5e97734a7",
+    "url": "/Portfolio/static/media/instagram.39a7d16b.svg"
+  },
+  {
+    "revision": "10f49fcbf7bf3d2b0fb45463a4dacb5b",
+    "url": "/Portfolio/static/media/istockphoto-1060210738-1024x1024.10f49fcb.jpg"
+  },
+  {
+    "revision": "ce7717ab2b9a5a0a9899a83636d70140",
+    "url": "/Portfolio/static/media/linkedin.ce7717ab.svg"
+  },
+  {
+    "revision": "eeeb54178b0817c65fa0d2c1e59df11a",
+    "url": "/Portfolio/static/media/nodejs.eeeb5417.svg"
+  },
+  {
+    "revision": "4da183294702522fdf8ec8e96fa8c31c",
+    "url": "/Portfolio/static/media/photo_2020-02-06_16-17-46.4da18329.jpg"
+  },
+  {
+    "revision": "b0276c68102df73bd7399e9de9a2881e",
+    "url": "/Portfolio/static/media/react.b0276c68.png"
+  },
+  {
+    "revision": "a385868eee1c6183119b932d5b89eb0f",
+    "url": "/Portfolio/static/media/redux.a385868e.png"
+  },
+  {
+    "revision": "e1f61d127fe8be1bd5315564c3e27f82",
+    "url": "/Portfolio/static/media/telegram.e1f61d12.svg"
+  },
+  {
+    "revision": "22ac85032e0b5cfca26b7fa695d5be1e",
+    "url": "/Portfolio/static/media/todolist.22ac8503.jpg"
+  },
+  {
+    "revision": "5645a12aeb7e4ce27ae5088accb7e927",
+    "url": "/Portfolio/static/media/vk.5645a12a.svg"
+  }
+]);
