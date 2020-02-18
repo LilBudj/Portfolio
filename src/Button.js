24229c2ce5b27a2f5f5258e249fd56500a8ae877
LilBudj <kinich2001@gmail.com>
2020-02-18 23:39:03 +0300

    connected to heroku server

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import style from './Button.module.css'
 
 const Button = (props) => {
     return(
-        <button className={style.button}>{props.content}</button>
+        <button className={style.button} onClick={() => {props.action(props.event)}}>{props.content}</button>
     )
 };
 
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Contacts.module.css';
 import Mark from "./RedMark";
-import Button from "./Button";
 import Fade from "react-reveal";
+import axios from "axios";
 
 const Contacts = () => {
+    const [email, setEmail] = useState(null);
+    const [name, setName] = useState(null);
+    const [message, setMessage] = useState(null);
+    const [isSent, setIsSent] = useState(false);
+
+    const sendMessage = () => {
+        if (email && name) {
+            axios.post('https://smtp-nodejs.herokuapp.com/', {email, name, message}).then(
+                (res) => {console.log(res)}
+            );
+            setIsSent(true)
+        }
+    };
+
     return(
         <div className={style.contacts} id={'Contacts'}>
             <Fade bottom>
             <div className={style.container}>
                 <Mark mark={"Contacts"}/>
-               <form className={style.form}>
-                   <input className={style.formArea} placeholder={'Email'}/>
-                   <input className={style.formArea} placeholder={'Name'}/>
-                   <textarea className={style.inputArea} placeholder={'Your message'}/>
-                   <Button content={'Submit'}/>
-               </form>
+                {!isSent ? <form id={"contact"} className={style.form}>
+                   <input className={style.formArea} placeholder={'Email'} name={"email"} form={"#contact"} value={email} onChange={(e) => {setEmail(e.currentTarget.value)}}/>
+                   <input className={style.formArea} placeholder={'Name'} name={"name"} form={"#contact"} value={name} onChange={(e) => {setName(e.currentTarget.value)}}/>
+                   <textarea className={style.inputArea} placeholder={'Your message'} name={"message"} form={"#contact"} value={message} onChange={(e) => {setMessage(e.currentTarget.value)}}/>
+                   <button className={style.button} onClick={() => {sendMessage()}} form={"#contact"}>Submit</button>
+               </form> : "Thank You!"}
             </div>
             </Fade>
         </div>
